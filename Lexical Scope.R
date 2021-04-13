ece1e3c3555e0734658cea074fdac919e5c710c7
MathewCortes <82426027+MathewCortes@users.noreply.github.com>
2021-04-13 23:58:03 +0800

    Part 2

--- Lexical Scope.R
+++ Lexical Scope.R
@@ -1,3 +1,9 @@
+  ## Put comments here that give an overall description of what your
+  ## functions do
+  
+  ##We have a function which consist of makeCacheMatrix,makeCacheMatrix
+  
+
 makeCacheMatrix <- function(x = matrix()) {
   
   i <- NULL
@@ -17,14 +23,14 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-
-
+## costly of its computation may have been some caching of benefits
+## with the inverse of the matrix on the other hand it may repeatedly compute
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
 }
 i <- x$getinverse()
 if(!is.null(i)) {
-  message("getting cached results")
+  message("getting cached data")
   return(i)
 }
 m <- x$get()
