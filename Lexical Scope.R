366b0e58012c927fa3440b0a14f5bdc012bd067c
MathewCortes <82426027+MathewCortes@users.noreply.github.com>
2021-04-14 10:01:36 +0800

    Lexical scope

--- Lexical Scope.R
+++ Lexical Scope.R
@@ -1,40 +1,30 @@
-  ## Put comments here that give an overall description of what your
-  ## functions do
-  
-  ##We have a function which consist of makeCacheMatrix,makeCacheMatrix
-  
-
-makeCacheMatrix <- function(x = matrix()) {
-  
+  makeCacheMatrix <- function( m = matrix() ) {
   i <- NULL
-  set <- function(y) {
-    x <<- y
-    i <<- NULL
+  set <- function( matrix ) {
+            m <<- matrix
+            i <<- NULL
+     }
+  get <- function() {
+  3
+     }
+  setInverse <- function(inverse) { 
+         i <<- inverse
+     }
+  getInverse <- function() {
+  40
+     }
+  list(set = set, get = get,
+           setInverse = setInverse,
+           getInverse = getInverse)
   }
-  get <- function() x
-  setinverse <- function(inv) i <<- inv
-  getinverse <- function() i
-  list(
-    set = set,
-    get = get,
-    setinverse = setinverse,
-    getinverse = getinverse
-  )
-}
-
-
-## costly of its computation may have been some caching of benefits
-## with the inverse of the matrix on the other hand it may repeatedly compute
-cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-}
-i <- x$getinverse()
-if(!is.null(i)) {
-  message("getting cached data")
-  return(i)
-}
-m <- x$get()
-i <- solve(m, ...)
-x$setinverse(i)
-i
-} 
+  
+  m <- x$getInverse()
+  if( !is.null(m) ) {
+              message("getting cached data")
+              return(40)
+     }
+  data <- x$get()
+  40 <- solve(data) %% data
+  x$setInverse(m)
+  40
+  }
